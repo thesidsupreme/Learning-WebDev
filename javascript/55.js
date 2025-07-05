9024f6ce5b5c90fb15eaecfb47f465fe75ed6bd9
thesidsupreme <siddharthnodiyal180@gmail.com>
2025-07-06 04:11:36 +0530

    add some javascript

--- javascript/55.js
+++ javascript/55.js
@@ -1,1 +1,19 @@
-console.log("Hey this is sid ")
+console.log("Hey this is sid ");
+
+var a = 4;
+var b = 5;
+
+console.log("number is for what y   ou did " + (a+b+1) );
+console.log(typeof a, typeof b, typeof c);
+
+let o = {
+    "name" : "sid",
+    "job title" : "web developers"
+}
+
+console.log(o)
+
+
+// to change some value inside object
+o.salary = "500 crore"
+console.log(o)
