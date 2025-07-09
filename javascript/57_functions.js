7e5884daef373a40fd9127f0f17dd08b6f5c0cf8
thesidsupreme <siddharthnodiyal180@gmail.com>
2025-07-10 04:37:14 +0530

    learned functions

---
+++ javascript/57_functions.js
@@ -0,0 +1,25 @@
+function greeting(name) {
+
+    console.log("Hey"+ name +"how are you?")
+    console.log(name +"how you been?")
+    
+}
+
+greeting(" sid ")
+
+
+function sumof(a,b) {
+    return a+b;
+}
+
+console.log("sum of these two number is  "+ sumof(10,20))
+
+
+
+const func1 = (x) =>{
+    console.log("i am a arrow function", x)
+}
+
+func1(2);
+func1(4)
+func1(5)
