2f529f1ee14ad9be7e10ebe37163accef8d19b32
thesidsupreme <siddharthnodiyal180@gmail.com>
2025-07-10 04:12:04 +0530

    adding loops

---
+++ javascript/56_loops.js
@@ -0,0 +1,25 @@
+for (let i = 1; i <= 100; i++) {
+    console.log(i)
+
+}
+
+let obj = {
+    first: "kiss",
+    second: "love",
+    third: "idk"
+}
+
+for (const key in obj) {
+    console.log(key)
+}
+
+for (const element of "Siddharth") {
+    console.log(element)
+}
+
+let i = 1;
+
+while (i <= 10) {
+    console.log(i)
+    i++;
+}
