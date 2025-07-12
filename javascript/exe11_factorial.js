29a3a777b6fee7a6a2d23dfe18d8ee78dc3965ed
thesidsupreme <siddharthnodiyal180@gmail.com>
2025-07-12 19:44:25 +0530

    done factorial with loops and will do with reduce later

---
+++ javascript/exe11_factorial.js
@@ -0,0 +1,30 @@
+// we have to calculate factorial using reduce() and for loop;
+
+
+
+
+// first with for loop
+
+
+var num = prompt("Enter the number you want factorial of")
+num = parseInt(num)
+let fact = 1;
+
+// for (let i = 1; i <= num; i++) {
+    
+//     fact = fact*i;
+
+    
+// }
+
+// console.log(fact)
+
+
+let redfact = (num,fact)=>{
+    fact*num
+}
+
+
+
+
+
