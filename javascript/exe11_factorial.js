649efa4ffff647bf9382cf93af7909224ed7b27f
thesidsupreme <siddharthnodiyal180@gmail.com>
2025-07-16 02:04:32 +0530

    factorial using reduce and for loops

--- javascript/exe11_factorial.js
+++ javascript/exe11_factorial.js
@@ -10,21 +10,34 @@ var num = prompt("Enter the number you want factorial of")
 num = parseInt(num)
 let fact = 1;
 
-// for (let i = 1; i <= num; i++) {
+for (let i = 1; i <= num; i++) {
     
-//     fact = fact*i;
+    fact = fact*i;
 
     
-// }
-
-// console.log(fact)
-
-
-let redfact = (num,fact)=>{
-    fact*num
 }
 
+console.log(fact)
 
 
 
 
+
+
+
+
+let a = 6;
+
+function factorial (number) {
+    let arr = Array.from(Array(number + 1).keys()) // for making array without loop
+    console.log(arr.slice(1,))                  // This will print array from 1 to 6
+
+    let c = arr.slice(1,).reduce((a,b)=>{
+        return a*b;
+    })
+
+    console.log(c)
+    
+}
+
+factorial(3)
--- javascript/tempCodeRunnerFile.js
+++ javascript/tempCodeRunnerFile.js
@@ -1,3 +1,16 @@
-/ to change some value inside object
-o.salary = "500 crore"
-console.log(o)
+
+let a = 6;
+
+function factorial (number) {
+    let arr = Array.from(Array(number + 1).keys()) // for making array without loop
+    console.log(arr.slice(1,))                  // This will print array from 1 to 6
+
+    let c = arr.slice(1,).reduce((a,b)=>{
+        return a*b;
+    })
+
+    console.log(c)
+    
+}
+
+factorial(3)
