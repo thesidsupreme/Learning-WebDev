a93a235f47a4fdc85b5f283a9543386e7e8de8a8
thesidsupreme <siddharthnodiyal180@gmail.com>
2025-07-16 02:29:55 +0530

    made random colour boxes via javascript selector

---
+++ javascript/exe12.js
@@ -0,0 +1,99 @@
+console.log("har har mahadev")
+
+let rand = Math.random()
+
+if (rand <= 0.2) {
+
+    document.getElementById("1").style.backgroundColor = "aqua"
+
+} else if (rand > 0.2 && rand <= 0.4) {
+    document.getElementById("1").style.backgroundColor = "green"
+
+} else if (rand > 0.4 && rand <= 0.6) {
+    document.getElementById("1").style.backgroundColor = "red"
+} else if (rand > 0.6 && rand <= 0.8) {
+    document.getElementById("1").style.backgroundColor = "blue"
+} else {
+    document.getElementById("1").style.backgroundColor = "orange"
+
+}
+
+
+
+rand = Math.random()
+
+if (rand <= 0.2) {
+
+    document.getElementById("2").style.backgroundColor = "aqua"
+
+} else if (rand > 0.2 && rand <= 0.4) {
+    document.getElementById("2").style.backgroundColor = "green"
+
+} else if (rand > 0.4 && rand <= 0.6) {
+    document.getElementById("2").style.backgroundColor = "red"
+} else if (rand > 0.6 && rand <= 0.8) {
+    document.getElementById("2").style.backgroundColor = "blue"
+} else {
+    document.getElementById("2").style.backgroundColor = "orange"
+
+}
+
+
+
+rand = Math.random()
+
+if (rand <= 0.2) {
+
+    document.getElementById("3").style.backgroundColor = "aqua"
+
+} else if (rand > 0.2 && rand <= 0.4) {
+    document.getElementById("3").style.backgroundColor = "green"
+
+} else if (rand > 0.4 && rand <= 0.6) {
+    document.getElementById("3").style.backgroundColor = "red"
+} else if (rand > 0.6 && rand <= 0.8) {
+    document.getElementById("3").style.backgroundColor = "blue"
+} else {
+    document.getElementById("3").style.backgroundColor = "orange"
+
+}
+
+
+
+rand = Math.random()
+
+if (rand <= 0.2) {
+
+    document.getElementById("4").style.backgroundColor = "aqua"
+
+} else if (rand > 0.2 && rand <= 0.4) {
+    document.getElementById("4").style.backgroundColor = "green"
+
+} else if (rand > 0.4 && rand <= 0.6) {
+    document.getElementById("4").style.backgroundColor = "red"
+} else if (rand > 0.6 && rand <= 0.8) {
+    document.getElementById("4").style.backgroundColor = "blue"
+} else {
+    document.getElementById("4").style.backgroundColor = "orange"
+
+}
+
+
+
+rand = Math.random()
+
+if (rand <= 0.2) {
+
+    document.getElementById("5").style.backgroundColor = "aqua"
+
+} else if (rand > 0.2 && rand <= 0.4) {
+    document.getElementById("5").style.backgroundColor = "green"
+
+} else if (rand > 0.4 && rand <= 0.6) {
+    document.getElementById("5").style.backgroundColor = "red"
+} else if (rand > 0.6 && rand <= 0.8) {
+    document.getElementById("5").style.backgroundColor = "blue"
+} else {
+    document.getElementById("5").style.backgroundColor = "orange"
+
+}
