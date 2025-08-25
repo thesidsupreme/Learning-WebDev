b35048006d8c6510ca17385523d22724818fa3f7
thesidsupreme <siddharthnodiyal180@gmail.com>
2025-08-26 01:45:09 +0530

    did some javascript

--- javascriptchaiandcode/09_array.js
+++ javascriptchaiandcode/09_array.js
@@ -3,5 +3,11 @@
 let myarr = [0,2,3,4,5]
 let myarr2 = new Array(1,3,"sid")
 
-console.log(myarr)
+// console.log(myarr)
 console.log(myarr2)
+
+myarr.push(8)
+myarr.push(9)
+console.log(myarr)
+myarr.pop()
+console.log(myarr)
