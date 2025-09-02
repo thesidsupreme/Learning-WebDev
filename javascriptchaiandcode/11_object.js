9adf2157ceb1693b1119a25afb00e739586c37e2
thesidsupreme <siddharthnodiyal180@gmail.com>
2025-09-03 03:28:50 +0530

    idk

---
+++ javascriptchaiandcode/11_object.js
@@ -0,0 +1,10 @@
+// objects litreals
+
+const names = {
+    name1: "sid",
+    name2: "tannu",
+    name3: "saksham",
+    name4: "sejal"
+}
+
+console.log(names["name1"])
---
+++ tempCodeRunnerFile.javascript
@@ -0,0 +1,1 @@
+console.log("harr harr mahadev")
