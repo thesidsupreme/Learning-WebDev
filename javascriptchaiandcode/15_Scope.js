f6083084af96f64c952a5dd6366e470ed4d4f3d6
thesidsupreme <siddharthnodiyal180@gmail.com>
2025-09-05 17:08:29 +0530

    scope in js:

---
+++ javascriptchaiandcode/15_Scope.js
@@ -0,0 +1,20 @@
+function one(){
+    let username = "Sid"
+    function two(){
+        let website = "TheSidSupreme.com"
+        // console.log(username);
+        
+    }
+    // console.log(website);
+    
+}
+
+// console.log(one())
+
+
+const sumtwo = function (num){
+    return num + 1;
+
+}
+
+console.log(sumtwo(1))
