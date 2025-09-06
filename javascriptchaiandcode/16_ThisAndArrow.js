f7cb1b82e51f8609a9bffa8326d162e353309aaa
thesidsupreme <siddharthnodiyal180@gmail.com>
2025-09-06 23:29:57 +0530

    js

---
+++ javascriptchaiandcode/16_ThisAndArrow.js
@@ -0,0 +1,33 @@
+
+// const userData = {
+//     username: "SId",
+//     email: "Siddharthnodiyal@gmail"
+// ,
+//     welcomeMsg: function() {
+//         console.log(`hey how are you ${this.username}`)
+//     }
+// }
+
+// userData.welcomeMsg()       // sid
+// userData.username = "kala"
+// userData.welcomeMsg()// kala
+
+
+const sumtwo = (num1,num2) => {
+  return num1+num2;
+
+}
+
+console.log(sumtwo(10,20))
+
+
+
+
+
+
+const addTwo = (num1,num2) => num1+num2
+
+
+console.log(addTwo(10,20));
+
+
---
+++ javascriptchaiandcode/17_IIFE.js
@@ -0,0 +1,20 @@
+// let a = "kallu"
+
+
+// function sid() {
+//     let a = "siddharth"
+//     function print() {
+//         console.log(a)
+//     }
+//     print()
+// }
+
+// console.log 
+
+
+(function Sid(){
+    console.log("Database is connected")
+})();    
+
+
+
