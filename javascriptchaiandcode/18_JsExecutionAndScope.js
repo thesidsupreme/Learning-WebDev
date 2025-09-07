662a726da42f7d51f57766ecc28a86232da71dc7
thesidsupreme <siddharthnodiyal180@gmail.com>
2025-09-08 01:18:26 +0530

    execution in js

---
+++ javascriptchaiandcode/18_JsExecutionAndScope.js
@@ -0,0 +1,10 @@
+val1 = 10;                  // memory created val1 - undefined
+val2 = 20;
+
+function addNum(num1,num2) {        // addnum = defination (wholefunction)
+    return num1+num2;
+
+}
+
+console.log(addNum(val1,val2));         //undefined """"""""""""""""""""""""""
+console.log(addNum(10,2));              //undefined in memory creation cycles
