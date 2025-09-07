f3b10ed8e7dcc31f2d99d9c5635019ba65b5165f
thesidsupreme <siddharthnodiyal180@gmail.com>
2025-09-08 01:53:01 +0530

    flowOfprogram

---
+++ javascriptchaiandcode/19_flowOfProgram.js
@@ -0,0 +1,34 @@
+if (true) {
+    console.log("statement is running")
+}
+
+let realWeek = 3;
+
+switch (realWeek) {
+    case 1: 
+    console.log("Monday");  
+        break;
+    case 2: 
+    console.log("Tuesday");  
+        break;
+    case 3: 
+    console.log("Wednesday");  
+        break;
+    case 4: 
+    console.log("Thrusday");  
+        break;
+    case 5: 
+    console.log("Friday");  
+        break;
+    case 6: 
+    console.log("Saturday");  
+        break;
+    case 7: 
+    console.log("Sunday");  
+        break;
+   
+
+    default:
+        console.log("no match")
+        break;
+}
