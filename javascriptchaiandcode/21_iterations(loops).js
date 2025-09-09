20d0b98e408886d3bec1ba9ec6ce3f71780c21be
thesidsupreme <siddharthnodiyal180@gmail.com>
2025-09-10 03:31:13 +0530

    iterations js:

---
+++ javascriptchaiandcode/21_iterations(loops).js
@@ -0,0 +1,18 @@
+let SidsArray = ["devil, zaider, king of wolves, egoist"]
+
+for (let i = 0; i < SidsArray.length; i++) {
+    const element = SidsArray[i];
+
+    console.log(element);
+    console.log(typeof element)
+    
+    
+}
+
+let index =0;
+
+while (index<=10) {
+    console.log(index)
+    index++
+
+}
